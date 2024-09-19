2b4f236c0dd170e50fa4f9b46c9e5e47ef5f0bb9
michaelrwx <michaelsimonelli11@gmail.com>
2024-09-19 19:01:31 -0400

    Edited test file, good way to end lesson for the day. BTW I did all of this via the terminal, making the file in Vim! :D

---
+++ fundamentals/testfile.py
@@ -0,0 +1,23 @@
+##**Operators**
+## ** = Exponent, % = Modulars, // = Integer division, floored quotent (22//8 =2) 
+## / = Division, * = Multiplication, - = Subtraction, + = Addition
+
+##**Data Types**
+## integers = 1, -1
+## Floating-point numbers: 1.0, 1.25, -1.25
+## Strings = 'a', 'aaa'
+
+
+print('Alice' * 5) # Will print out "AliceAliceAliceAliceAlice"
+
+#Variables!
+spam = 42 #Integer variable
+youch = "Yowie zowie!"
+cooool = 67.8
+
+print("What is your age?")
+
+age = input() #Takes user input in command line
+
+print("You will be " + str(int(age) + 1) + " in one year!") #Showing how these work together
+
