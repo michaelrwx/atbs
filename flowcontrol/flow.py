4246d2386e5458362dadaee895cfe94f1572223c
michaelrwx <michaelsimonelli11@gmail.com>
2024-09-20 17:59:10 -0400

    Chapter 2's file

---
+++ flowcontrol/flow.py
@@ -0,0 +1,11 @@
+## == is whether two values are the same. = Puts the variable on the right into the variable on the left
+
+## if statements help flow control. Elif statements are else if statements
+
+name = ''
+
+while name != 'your name': #This forces the program to not stop until "your name" is typed verbatim.
+    print('Please type your name.')
+    name = input()
+
+print('Thank you!')
