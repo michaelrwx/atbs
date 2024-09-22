e350263ac720cd3b9beca30665b14bb713e59eac
michaelrwx <michaelsimonelli11@gmail.com>
2024-09-22 19:26:52 -0400

    finished lesson

--- flowcontrol/flow.py
+++ flowcontrol/flow.py
@@ -2,10 +2,37 @@
 
 ## if statements help flow control. Elif statements are else if statements
 
-name = ''
+#name = ''
 
-while name != 'your name': #This forces the program to not stop until "your name" is typed verbatim.
-    print('Please type your name.')
-    name = input()
+#while name != 'your name': #This forces the program to not stop until "your name" is typed verbatim.
+#    print('Please type your name.')
+#    name = input()
 
-print('Thank you!')
+#print('Thank you!')
+
+#there is a not operator
+#not True = False
+#not False = True
+
+name = 'Mary'
+password = 'swordfish'
+
+if name == 'Mary':
+    print('Hello, Mary')
+    if password == 'swordfish':
+        print('Access granted!')
+    else:
+        print('Wrong password')
+
+#there are elif statements. Or ELSE IF statements!!!
+
+#while loops are blocks of code that let a condition run until it is false
+
+#a break statement stops a while loop if theres a certain condition
+
+
+
+
+
+    
+    
