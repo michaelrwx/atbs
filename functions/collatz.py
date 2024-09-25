6c8ca12ef370d5606daa05cd5fa447719f58aaf7
michaelrwx <michaelsimonelli11@gmail.com>
2024-09-25 17:42:24 -0400

    Fixed collatz program

--- functions/collatz.py
+++ functions/collatz.py
@@ -1,23 +1,22 @@
-import sys
-
 def collatz(x):
-    if (x%2 == 0):
-        x = x // 2
-        return(x)
-    elif (x%2 == 1):
-        x = 3 * x + 1
-        return(x)
 
-print('Enter number: ')
-number = input()
+    if x % 2 == 0:
+        print(x // 2)
+        return(x // 2)
+    
+    elif x % 2 == 1:
+        result = 3 * x + 1
+        print(result)
+        return(result)
 
-while(number != 1):
-    collatz(int(number))
-    print(number)
+number = input("Enter a number: ")
+while number != 1:
+    number = collatz(int(number)) 
 
-if(number == 1):
-   print('1 acquired')
-   sys.exit()
+#You pretty much had the program, you just forgot to have this variable change within the variable.
+
+#The program defines the collatz definition. The function is used to print each number.
+
 
 
 
